2e05c949281b52ed2c4ad5a9b226ed4d6c2e4fb8
Andrew <andrew.charabin@voices.com>
2023-03-16 10:15:16 -0400

    adding playacvoice.py

---
+++ playacvoice.py
@@ -0,0 +1,10 @@
+import json
+import numpy as np
+import requests
+from IPython.display import Audio
+
+def play_ac_voice(text, samplingrate=22050, maxwavvalue=32768, api="http://159.203.49.85:5000/predict"):
+    response = requests.post(f"{api}", json={"text":text})
+    data=np.array(response.json())
+    scaled = np.int32(data / np.max(np.abs(data)) * maxwavvalue)
+    return Audio(scaled, rate=samplingrate, autoplay=True)
