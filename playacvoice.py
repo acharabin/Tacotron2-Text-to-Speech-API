f90d6f08d1c9bf7146833bc403c2dea309b54eef
Andrew <andrew.charabin@voices.com>
2023-03-17 10:25:38 -0400

    adding maxcharacters parameter to playacvoice

--- playacvoice.py
+++ playacvoice.py
@@ -3,7 +3,8 @@ import numpy as np
 import requests
 from IPython.display import Audio
 
-def play_ac_voice(text, samplingrate=22050, maxwavvalue=32768, api="http://159.203.49.85:5000/predict"):
+def play_ac_voice(text, samplingrate=22050, maxwavvalue=32768, api="http://159.203.49.85:5000/predict", maxcharacters=100):
+    if len(text)>maxcharacters: return print(f"""inputted text length above maximum value of {maxcharacters}""")
     response = requests.post(f"{api}", json={"text":text})
     data=np.array(response.json())
     scaled = np.int32(data / np.max(np.abs(data)) * maxwavvalue)
