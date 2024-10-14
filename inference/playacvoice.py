def18d818ac32d17cb1c71750456b8515345fed4
Andrew Charabin <122898165+acharabin@users.noreply.github.com>
2024-10-14 19:47:16 -0400

    Update playacvoice.py
    Test adding User-Agent

--- inference/playacvoice.py
+++ inference/playacvoice.py
@@ -5,7 +5,7 @@ from IPython.display import Audio
 
 def play_ac_voice(text, samplingrate=22050, maxwavvalue=32768, api="http://159.203.49.85:5000/predict", maxcharacters=200):
     if len(text)>maxcharacters: return print(f"""inputted text length above maximum value of {maxcharacters}""")
-    response = requests.post(f"{api}", json={"text":text})
+    response = requests.post(f"{api}", json={"text":text},headers = { 'User-Agent': 'My User Agent 1.0'})
     data=np.array(response.json())
     scaled = np.int32(data / np.max(np.abs(data)) * maxwavvalue)
     return Audio(scaled, rate=samplingrate, autoplay=True)
